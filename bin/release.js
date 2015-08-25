e33e4763b850ae401e1fac25d6cd30e95dc14cef
Lars Kappert <lars@webpro.nl>
2015-08-25 22:16:16 +0200

    Improved error/exit handling

--- bin/release.js
+++ bin/release.js
@@ -3,9 +3,20 @@
 var release = require('../lib/release'),
     args = [].slice.call(process.argv, 2);
 
+var exitCode = 0,
+    isDebug = args.indexOf('--debug') !== -1;
+
 release.cli(args).then(function() {
-    process.exit(0);
-}, function(error) {
-    console.error(error);
-    process.exit(1);
+    process.exit(exitCode);
+}).catch(function(err) {
+    exitCode = 1;
+    if(!isDebug) {
+        console.error(error);
+    } else {
+        throw new Error(err)
+    }
+});
+
+process.on('exit', function() {
+    process.exit(exitCode);
 });
