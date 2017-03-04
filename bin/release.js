4f4bc436e69a4029fc5d72f8125b6ccc9f009fcc
Lars Kappert <lars@webpro.nl>
2017-03-04 12:23:06 +0100

    Format bin & config

--- bin/release.js
+++ bin/release.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
-var release = require('../lib/release'),
-    args = [].slice.call(process.argv, 2);
+const release = require('../lib/release'),
+  args = [].slice.call(process.argv, 2),
+  isDebug = args.indexOf('--debug') !== -1;
 
-var exitCode = 0,
-    isDebug = args.indexOf('--debug') !== -1;
+var exitCode = 0;
 
 release.cli(args).then(function() {
-    process.exit(exitCode);
+  process.exit(exitCode);
 }).catch(function(err) {
-    exitCode = 1;
-    if(!isDebug) {
-        console.error(err);
-    } else {
-        throw new Error(err);
-    }
+  exitCode = 1;
+  if(!isDebug) {
+    console.error(err);
+  } else {
+    throw new Error(err);
+  }
 });
 
 process.on('exit', function() {
-    process.exit(exitCode);
+  process.exit(exitCode);
 });
