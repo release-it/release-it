60b2dffcfa00732789e0ab9759a3eee441eb1a53
Lars Kappert <lars@webpro.nl>
2017-11-05 12:33:18 +0100

    Add update-notifier

--- bin/release-it.js
+++ bin/release-it.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+var updater = require('update-notifier');
+var pkg = require('../package.json');
+
 require('babel-register')({
   ignore: false,
   only: /release-it\/lib/
@@ -7,4 +10,5 @@ require('babel-register')({
 
 const release = require('../lib/index').default;
 
+updater({ pkg: pkg }).notify();
 release().then(() => process.exit(0), () => process.exit(1));
