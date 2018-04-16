d3adb01208624eb75b0376fcffb165b9a1f8419f
Lars Kappert <lars@webpro.nl>
2018-04-16 21:40:16 +0200

    Add custom error classes + fix up tests for tasks

--- test/tasks.js
+++ test/tasks.js
@@ -1,40 +1,48 @@
-const tasks = require('../lib/tasks');
 const test = require('tape');
 const shell = require('shelljs');
-const { run, copy } = require('../lib/shell');
+const proxyquire = require('proxyquire');
+const { Config } = require('../lib/config');
+const { run } = require('../lib/shell');
+const {
+  GitRepoError,
+  GitRemoteUrlError,
+  GitCleanWorkingDirError,
+  GitUpstreamError,
+  GithubTokenError,
+  VersionNotFoundError
+} = require('../lib/errors');
+
+const getMock = config =>
+  proxyquire('../lib/tasks', {
+    './config': { config }
+  });
 
 const tmp = 'test/resources/tmp';
 
-test('test not a git repository', async t => {
+test('should throw when not a Git repository', async t => {
+  const tasks = getMock(new Config());
   shell.pushd('-q', '..');
-
-  await tasks({}).catch(e => {
-    t.equal(e.code, 'NOT_GIT_REPO');
-  });
-
+  await t.shouldBailOut(tasks(), GitRepoError, /Not a git repository/);
   shell.popd('-q');
   t.end();
 });
 
-test('test no-remote-git-url error message', async t => {
-  shell.rm('-rf', tmp);
+test('should throw if there is no remote Git url', async t => {
+  const tasks = getMock(new Config());
   shell.mkdir(tmp);
   shell.pushd('-q', tmp);
   await run('git init');
   await run('touch file1');
   await run('git add file1');
   await run('git commit -am "Add file1"');
-
-  await tasks({}).catch(e => {
-    t.equal(e.code, 'NO_REMOTE_URL');
-  });
-
+  await t.shouldBailOut(tasks(), GitRemoteUrlError, /Could not get remote Git url/);
   shell.popd('-q');
   shell.rm('-rf', tmp);
   t.end();
 });
 
-test('test working-dir-must-be-clean error message', async t => {
+test('should throw if working dir is not clean', async t => {
+  const tasks = getMock(new Config());
   shell.rm('-rf', tmp);
   shell.mkdir(tmp);
   shell.pushd('-q', tmp);
@@ -42,17 +50,18 @@ test('test working-dir-must-be-clean error message', async t => {
   await run('touch file1');
   await run('git remote add origin foo');
   await run('git add file1');
-
-  await tasks({}).catch(e => {
-    t.equal(e.code, 'WORKING_DIR_MUST_BE_CLEAN');
-  });
-
+  await t.shouldBailOut(tasks(), GitCleanWorkingDirError, /Working dir must be clean/);
   shell.popd('-q');
   shell.rm('-rf', tmp);
   t.end();
 });
 
-test('test has upstream error message', async t => {
+test('should throw if no upstream is configured for current branch', async t => {
+  const tasks = getMock(
+    new Config({
+      requireUpstream: true
+    })
+  );
   shell.rm('-rf', tmp);
   shell.mkdir(tmp);
   shell.pushd('-q', tmp);
@@ -61,19 +70,22 @@ test('test has upstream error message', async t => {
   await run('git remote add origin foo');
   await run('git add file1');
   await run('git commit -am "Add file1"');
-
-  await tasks({
-    requireUpstream: true
-  }).catch(e => {
-    t.equal(e.code, 'NO_UPSTREAM');
-  });
-
+  await t.shouldBailOut(tasks(), GitUpstreamError, /No upstream configured for current branch/);
   shell.popd('-q');
   shell.rm('-rf', tmp);
   t.end();
 });
 
-test('test no github token', async t => {
+test('should throw if no GitHub token environment variable is set', async t => {
+  const tasks = getMock(
+    new Config({
+      requireUpstream: false,
+      github: {
+        release: true,
+        tokenRef: 'GITHUB_FOO'
+      }
+    })
+  );
   shell.rm('-rf', tmp);
   shell.mkdir(tmp);
   shell.pushd('-q', tmp);
@@ -82,13 +94,29 @@ test('test no github token', async t => {
   await run('git remote add origin foo');
   await run('git add file1');
   await run('git commit -am "Add file1"');
+  await t.shouldBailOut(tasks(), GithubTokenError, /Environment variable "GITHUB_FOO" is required for GitHub releases/);
+  shell.popd('-q');
+  shell.rm('-rf', tmp);
+  t.end();
+});
 
-  await tasks({
-    requireUpstream: false
-  }).catch(e => {
-    t.equal(e.code, 'NO_GITHUB_TOKENREF');
-  });
-
+test('should throw if invalid increment value is provided (in non-interactive mode)', async t => {
+  const tasks = getMock(
+    new Config({
+      increment: 'mini',
+      'non-interactive': true,
+      requireUpstream: false
+    })
+  );
+  shell.rm('-rf', tmp);
+  shell.mkdir(tmp);
+  shell.pushd('-q', tmp);
+  await run('git init');
+  await run('touch file1');
+  await run('git remote add origin foo');
+  await run('git add file1');
+  await run('git commit -am "Add file1"');
+  await t.shouldBailOut(tasks(), VersionNotFoundError, /no or an invalid version is provided/);
   shell.popd('-q');
   shell.rm('-rf', tmp);
   t.end();
--- test/util/setup.js
+++ test/util/setup.js
@@ -1,4 +1,5 @@
 const { Test } = require('tape');
+const mockStdIo = require('mock-stdio');
 
 const noop = () => {};
 
@@ -10,6 +11,21 @@ Test.prototype.shouldReject = function(promise, expected) {
         throw err;
       };
       this.throws(f, expected);
+    }
+  );
+};
+
+Test.prototype.shouldBailOut = function(promise, expected, expectedStdErr) {
+  mockStdIo.start();
+  return promise.then(
+    () => this.throws(noop, expected),
+    err => {
+      const { stderr } = mockStdIo.end();
+      const f = () => {
+        throw err;
+      };
+      this.throws(f, expected);
+      this.ok(expectedStdErr.test(stderr));
     }
   );
 };
