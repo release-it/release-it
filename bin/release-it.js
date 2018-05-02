26d7d4c9e0db19ec0781543dbe91100c82d622d5
Lars Kappert <lars@webpro.nl>
2018-05-02 20:32:53 +0200

    Only load babel-register for Node < 7

--- bin/release-it.js
+++ bin/release-it.js
@@ -2,10 +2,13 @@
 
 var updater = require('update-notifier');
 var pkg = require('../package.json');
+var semver = require('semver');
 
-require('babel-register')({
-  only: /release-it\/lib/
-});
+if (semver.major(process.version) < 7) {
+  require('babel-register')({
+    only: /release-it\/lib/
+  });
+}
 
 const release = require('../lib');
 
