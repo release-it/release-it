4cfd598602fd0a50f06981578ca703b3d11e1057
Lars Kappert <lars@webpro.nl>
2018-12-27 23:53:32 +0100

    Add `use` option to take latest version from `git.tag` or `pkg.version`

--- test/version.js
+++ test/version.js
@@ -17,92 +17,154 @@ test('isPreRelease', t => {
   t.end();
 });
 
+test('setLatestVersion', t => {
+  const v = new Version();
+  v.setLatestVersion({ gitTag: '1.2.0' });
+  t.equal(v.latestVersion, '1.2.0');
+  v.setLatestVersion({ gitTag: '1.2.1', pkgVersion: '1.2.2' });
+  t.equal(v.latestVersion, '1.2.1');
+  v.setLatestVersion({ use: 'pkg.version', pkgVersion: '1.2.3' });
+  t.equal(v.latestVersion, '1.2.3');
+  t.end();
+});
+
+test('setLatestVersion (not root dir)', t => {
+  const v = new Version();
+  v.setLatestVersion({ gitTag: '1.2.3', pkgVersion: '1.2.4', isRootDir: false });
+  t.equal(v.latestVersion, '1.2.4');
+  t.end();
+});
+
+test('setLatestVersion (invalid tag/fallback)', t => {
+  const v = new Version();
+  mockStdIo.start();
+  v.setLatestVersion({ gitTag: 'a.b.c', pkgVersion: '0.0.1' });
+  const { stdout } = mockStdIo.end();
+  t.ok(/Latest Git tag \(a\.b\.c\) is not a valid semver version/.test(stdout));
+  t.equal(v.latestVersion, '0.0.1');
+  t.end();
+});
+
+test('setLatestVersion (invalid package version)', t => {
+  const v = new Version();
+  mockStdIo.start();
+  v.setLatestVersion({ use: 'pkg.version', pkgVersion: '1.2' });
+  const { stdout } = mockStdIo.end();
+  t.ok(/The version in package.json \(1\.2\) is not a valid semver version/.test(stdout));
+  t.end();
+});
+
+test('setLatestVersion (invalid git tag and package version)', t => {
+  const v = new Version();
+  mockStdIo.start();
+  v.setLatestVersion({ gitTag: '1', pkgVersion: '2' });
+  const { stdout } = mockStdIo.end();
+  t.ok(
+    /Could not find valid latest Git tag or version in package.json. Using "0\.0\.0" as latest version/.test(stdout)
+  );
+  t.end();
+});
+
 test('bump', async t => {
-  const v = new Version({ latestVersion: '2.2.0' });
+  const v = new Version();
+  v.setLatestVersion({ gitTag: '2.2.0' });
   await v.bump({ increment: 'patch' });
+  t.equal(v.latestVersion, '2.2.0');
   t.equal(v.version, '2.2.1');
   t.end();
 });
 
 test('bump (to provided version)', async t => {
-  const v = new Version({ latestVersion: '1.0.0' });
+  const v = new Version();
+  v.setLatestVersion({ gitTag: '1.0.0' });
   await v.bump({ increment: '1.2.3' });
   t.equal(v.version, '1.2.3');
   t.end();
 });
 
 test('bump (to lower version)', async t => {
-  const v = new Version({ latestVersion: '2.2.0' });
+  const v = new Version();
+  v.setLatestVersion({ gitTag: '2.2.0' });
   await v.bump({ increment: '0.8.0' });
   t.equal(v.version, undefined);
   t.end();
 });
 
 test('bump (null)', async t => {
-  const v = new Version({ latestVersion: '2.2.0' });
+  const v = new Version();
+  v.setLatestVersion({ gitTag: '2.2.0' });
   await v.bump({ increment: null });
   t.equal(v.version, undefined);
   t.end();
 });
 
 test('bump (patch pre-release)', async t => {
-  const v = new Version({ latestVersion: '0.2.0', preReleaseId: 'alpha' });
+  const v = new Version({ preReleaseId: 'alpha' });
+  v.setLatestVersion({ gitTag: '0.2.0' });
   await v.bump({ increment: 'prepatch', preRelease: true });
   t.equal(v.version, '0.2.1-alpha.0');
   t.end();
 });
 
 test('bump (patch pre-release normalized)', async t => {
-  const v = new Version({ latestVersion: '0.2.0', preReleaseId: 'alpha' });
+  const v = new Version({ preReleaseId: 'alpha' });
+  v.setLatestVersion({ gitTag: '0.2.0' });
   await v.bump({ increment: 'patch', preRelease: true });
   t.equal(v.version, '0.2.1-alpha.0');
   t.end();
 });
 
 test('bump (prerelease)', async t => {
-  const v = new Version({ latestVersion: '0.2.1', preReleaseId: 'alpha' });
+  const v = new Version({ preReleaseId: 'alpha' });
+  v.setLatestVersion({ gitTag: '0.2.1' });
   await v.bump({ increment: 'prerelease', preRelease: true });
   t.equal(v.version, '0.2.2-alpha.0');
   t.end();
 });
 
 test('bump (prepatch continuation)', async t => {
-  const v = new Version({ latestVersion: '0.2.1-alpha.0' });
+  const v = new Version();
+  v.setLatestVersion({ gitTag: '0.2.1-alpha.0' });
   await v.bump({ increment: 'prerelease' });
   t.equal(v.version, '0.2.1-alpha.1');
   t.end();
 });
 
 test('bump (preReleaseId continuation)', async t => {
-  const v = new Version({ latestVersion: '0.2.1-alpha.0', preReleaseId: 'alpha' });
+  const v = new Version({ preReleaseId: 'alpha' });
+  v.setLatestVersion({ gitTag: '0.2.1-alpha.0' });
   await v.bump({ increment: 'prerelease' });
   t.equal(v.version, '0.2.1-alpha.1');
   t.end();
 });
 
 test('bump (prepatch/preReleaseId continuation)', async t => {
-  const v = new Version({ latestVersion: '0.2.1-alpha.0', preReleaseId: 'alpha' });
+  const v = new Version({ preReleaseId: 'alpha' });
+  v.setLatestVersion({ gitTag: '0.2.1-alpha.0' });
   await v.bump({ increment: 'prerelease', preRelease: true });
   t.equal(v.version, '0.2.1-alpha.1');
   t.end();
 });
 
 test('bump (preReleaseId w/o preRelease)', async t => {
-  const v = new Version({ latestVersion: '0.2.1-alpha.0', preReleaseId: 'alpha' });
+  const v = new Version({ preReleaseId: 'alpha' });
+  v.setLatestVersion({ gitTag: '0.2.1-alpha.0' });
   await v.bump({ increment: 'patch' });
   t.equal(v.version, '0.2.1');
   t.end();
 });
 
 test('bump (non-numeric prepatch continuation)', async t => {
-  const v = new Version({ latestVersion: '0.2.1-alpha' });
+  const v = new Version();
+  v.setLatestVersion({ gitTag: '0.2.1-alpha' });
   await v.bump({ increment: 'prerelease' });
   t.equal(v.version, '0.2.1-alpha.0');
   t.end();
 });
 
 test('bump (patch release after pre-release)', async t => {
-  const v = new Version({ latestVersion: '0.2.1-alpha.1' });
+  const v = new Version();
+  v.setLatestVersion({ gitTag: '0.2.1-alpha.1' });
   await v.bump({ increment: 'patch' });
   t.equal(v.version, '0.2.1');
   t.end();
@@ -118,7 +180,8 @@ test('bump (recommended conventional)', async t => {
   sh.exec('echo line >> file && git add file && git commit -m "feat(foo): extend the foo"');
   sh.exec('echo line >> file && git add file && git commit -m "feat(bar): more bar"');
 
-  const v = new Version({ latestVersion: '1.0.0' });
+  const v = new Version();
+  v.setLatestVersion({ gitTag: '1.0.0' });
   await v.bump({ increment: 'conventional:angular' });
   t.equal(v.version, '1.1.0');
 
@@ -133,55 +196,31 @@ const recommendations = {
 };
 
 test('bump (recommended conventional w/ pre-release)', async t => {
-  const v = new Version({ latestVersion: '1.0.0', preReleaseId: 'canary', recommendations });
+  const v = new Version({ preReleaseId: 'canary', recommendations });
+  v.setLatestVersion({ gitTag: '1.0.0' });
   await v.bump({ increment: 'conventional:angular', preRelease: true });
   t.equal(v.version, '1.1.0-canary.0');
   t.end();
 });
 
 test('bump (recommended conventional w/o preRelease)', async t => {
-  const v = new Version({ latestVersion: '1.0.0', preReleaseId: 'canary', recommendations });
+  const v = new Version({ preReleaseId: 'canary', recommendations });
+  v.setLatestVersion({ gitTag: '1.0.0' });
   await v.bump({ increment: 'conventional:angular' });
   t.equal(v.version, '1.1.0');
   t.end();
 });
 
 test('bump (recommended conventional w/ pre-release continuation)', async t => {
-  const v = new Version({ latestVersion: '1.0.0-canary.1', preReleaseId: 'canary', recommendations });
+  const v = new Version({ preReleaseId: 'canary', recommendations });
+  v.setLatestVersion({ gitTag: '1.0.0-canary.1' });
   await v.bump({ increment: 'conventional:angular', preRelease: true });
   t.equal(v.version, '1.0.0-canary.2');
   t.end();
 });
 
-test('bump (invalid tag)', async t => {
-  const v = new Version();
-  mockStdIo.start();
-  v.showWarnings({ latestGitTag: 'a.b.c', npmVersion: '0.0.1', useTag: true });
-  const { stdout } = mockStdIo.end();
-  t.ok(/Latest Git tag \(a\.b\.c\) is not a valid semver version/.test(stdout));
-  t.end();
-});
-
-test('parse (invalid npm version)', async t => {
-  const v = new Version();
-  mockStdIo.start();
-  v.showWarnings({ latestGitTag: '2.2.0', npmVersion: '1.2' });
-  const { stdout } = mockStdIo.end();
-  t.ok(/The npm version \(1\.2\) is not a valid semver version/.test(stdout));
-  t.end();
-});
-
-test('bump (not matching)', async t => {
-  const v = new Version();
-  mockStdIo.start();
-  v.showWarnings({ latestGitTag: '1.0.0', npmVersion: '1.0.1', useTag: true });
-  const { stdout } = mockStdIo.end();
-  t.ok(/Latest Git tag \(1\.0\.0\) doesn't match package.json#version \(1\.0\.1\)/.test(stdout));
-  t.end();
-});
-
 test('parse (coerce)', async t => {
-  const v = new Version({ latestVersion: '1.1.0' });
+  const v = new Version();
   mockStdIo.start();
   v.bump({ increment: '2' });
   const { stdout } = mockStdIo.end();
