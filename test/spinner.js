3fcaf0ae926201f05a4d2a335d9dafdd34a8e8c7
Lars Kappert <lars@webpro.nl>
2019-01-10 13:47:58 +0100

    Add tests for `Spinner` class

---
+++ test/spinner.js
@@ -0,0 +1,48 @@
+const test = require('ava');
+const sinon = require('sinon');
+const Spinner = require('../lib/spinner');
+
+test.beforeEach(t => {
+  t.context.ora = {
+    promise: sinon.spy()
+  };
+});
+
+test('should not show spinner and not execute task if disabled', async t => {
+  const { ora } = t.context;
+  const task = sinon.spy();
+  const spinner = new Spinner({ ora });
+  await spinner.show({ enabled: false, task });
+  t.is(task.callCount, 0);
+  t.is(ora.promise.callCount, 0);
+});
+
+test('should show spinner and run task by default', async t => {
+  const { ora } = t.context;
+  const task = sinon.stub().resolves();
+  const label = 'foo';
+  const spinner = new Spinner({ ora });
+  await spinner.show({ task, label });
+  t.is(task.callCount, 1);
+  t.is(ora.promise.callCount, 1);
+  t.is(ora.promise.firstCall.args[0], task.firstCall.returnValue);
+  t.is(ora.promise.firstCall.args[1], label);
+});
+
+test('should run task, but not show spinner if interactive', async t => {
+  const { ora } = t.context;
+  const task = sinon.stub().resolves();
+  const spinner = new Spinner({ isInteractive: true, ora });
+  await spinner.show({ task });
+  t.is(task.callCount, 1);
+  t.is(ora.promise.callCount, 0);
+});
+
+test('should run task and show spinner if interactive, but forced', async t => {
+  const { ora } = t.context;
+  const task = sinon.stub().resolves();
+  const spinner = new Spinner({ isInteractive: true, ora });
+  await spinner.show({ task, forced: true });
+  t.is(task.callCount, 1);
+  t.is(ora.promise.callCount, 1);
+});
