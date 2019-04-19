bde14321f25320379064b2e3b0e921f8ad8e9779
Lars Kappert <lars@webpro.nl>
2019-04-19 18:12:37 +0200

    Set/reset GitHub/GitLab tokens during test run

--- test/util/setup.js
+++ test/util/setup.js
@@ -1,1 +1,11 @@
+const test = require('ava');
 require('shelljs').config.silent = true;
+
+const { GITHUB_TOKEN, GITLAB_TOKEN } = process.env;
+
+process.env.GITHUB_TOKEN = process.env.GITLAB_TOKEN = 1;
+
+test.after.always(() => {
+  process.env.GITHUB_TOKEN = GITHUB_TOKEN;
+  process.env.GITLAB_TOKEN = GITLAB_TOKEN;
+});
