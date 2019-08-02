406a23b73b8a8c9004d1bac41ae4ea0415bbc6cc
Lars Kappert <lars@webpro.nl>
2019-08-02 13:41:42 +0200

    Fix up tests and increase coverage for Log module

--- test/log.js
+++ test/log.js
@@ -1,5 +1,7 @@
+const { EOL } = require('os');
 const test = require('ava');
 const mockStdIo = require('mock-stdio');
+const stripAnsi = require('strip-ansi');
 const Log = require('../lib/log');
 
 test('should write to stdout', t => {
@@ -7,7 +9,7 @@ test('should write to stdout', t => {
   mockStdIo.start();
   log.log('foo');
   const { stdout, stderr } = mockStdIo.end();
-  t.is(stdout.trim(), 'foo');
+  t.is(stdout, 'foo\n');
   t.is(stderr, '');
 });
 
@@ -16,16 +18,16 @@ test('should write to stderr', t => {
   mockStdIo.start();
   log.error('foo');
   const { stdout, stderr } = mockStdIo.end();
-  t.is(stdout.trim(), '');
-  t.true(stderr.trim().endsWith('foo'));
+  t.is(stdout, '');
+  t.is(stripAnsi(stderr), 'ERROR foo\n');
 });
 
-test('should print warning', t => {
+test('should print a warning', t => {
   const log = new Log();
   mockStdIo.start();
   log.warn('foo');
   const { stdout } = mockStdIo.end();
-  t.true(stdout.trim().endsWith('foo'));
+  t.is(stripAnsi(stdout), 'WARNING foo\n');
 });
 
 test('should print verbose', t => {
@@ -33,7 +35,7 @@ test('should print verbose', t => {
   mockStdIo.start();
   log.verbose('foo');
   const { stdout } = mockStdIo.end();
-  t.is(stdout.trim(), 'foo');
+  t.is(stdout, 'foo\n');
 });
 
 test('should not print verbose by default', t => {
@@ -41,7 +43,7 @@ test('should not print verbose by default', t => {
   mockStdIo.start();
   log.verbose('foo');
   const { stdout } = mockStdIo.end();
-  t.is(stdout.trim(), '');
+  t.is(stdout, '');
 });
 
 test('should not print command execution by default', t => {
@@ -65,7 +67,7 @@ test('should print command execution (dry run)', t => {
   mockStdIo.start();
   log.exec('foo');
   const { stdout } = mockStdIo.end();
-  t.is(stdout.trim(), '$ foo');
+  t.is(stdout, '$ foo\n');
 });
 
 test('should print command execution (read-only)', t => {
@@ -73,7 +75,7 @@ test('should print command execution (read-only)', t => {
   mockStdIo.start();
   log.exec('foo', 'bar', false);
   const { stdout } = mockStdIo.end();
-  t.is(stdout.trim(), '$ foo bar');
+  t.is(stdout, '$ foo bar\n');
 });
 
 test('should print command execution (write)', t => {
@@ -81,5 +83,29 @@ test('should print command execution (write)', t => {
   mockStdIo.start();
   log.exec('foo', '--arg n', true);
   const { stdout } = mockStdIo.end();
-  t.is(stdout.trim(), '! foo --arg n');
+  t.is(stdout, '! foo --arg n\n');
+});
+
+test('should print obtrusive', t => {
+  const log = new Log({ isCI: false });
+  mockStdIo.start();
+  log.obtrusive('spacious');
+  const { stdout } = mockStdIo.end();
+  t.is(stdout, '\nspacious\n\n');
+});
+
+test('should not print obtrusive in CI mode', t => {
+  const log = new Log({ isCI: true });
+  mockStdIo.start();
+  log.obtrusive('normal');
+  const { stdout } = mockStdIo.end();
+  t.is(stdout, 'normal\n');
+});
+
+test('should print preview', t => {
+  const log = new Log();
+  mockStdIo.start();
+  log.preview({ title: 'title', text: 'changelog' });
+  const { stdout } = mockStdIo.end();
+  t.is(stripAnsi(stdout), `Title:${EOL}changelog\n`);
 });
