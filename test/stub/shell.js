36cbdec3b7b3d1aa9c270e5dc865ccb5d9ba2c24
Lars Kappert <lars@webpro.nl>
2019-12-08 20:04:17 +0100

    Move debug creator

--- test/stub/shell.js
+++ test/stub/shell.js
@@ -1,10 +1,10 @@
-const debug = require('debug');
+const debug = require('debug')('release-it:shell-stub');
 const Shell = require('../../lib/shell');
 
 module.exports = class ShellStub extends Shell {
   exec(command) {
     if (/^(npm|git fetch)/.test(command)) {
-      debug('release-it:shell-stub')(command);
+      debug(command);
       return Promise.resolve();
     }
     return super.exec(...arguments);
