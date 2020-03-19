f05150d567e6d8e07f4c756975a7c0ad957a4f89
Lars Kappert <lars@webpro.nl>
2020-03-19 08:31:23 +0100

    Use GITHUB_ACTIONS env var + un/set in test setup

--- test/util/setup.js
+++ test/util/setup.js
@@ -5,12 +5,13 @@ const test = require('ava');
 shelljs.config.silent = true;
 nock.disableNetConnect();
 
-const { GITHUB_TOKEN, GITLAB_TOKEN } = process.env;
+const { GITHUB_TOKEN, GITLAB_TOKEN, GITHUB_ACTIONS, GITHUB_ACTOR } = process.env;
 
 process.env.GITHUB_TOKEN = process.env.GITLAB_TOKEN = 1;
 
 test.after.always(() => {
   process.env.GITHUB_TOKEN = GITHUB_TOKEN;
   process.env.GITLAB_TOKEN = GITLAB_TOKEN;
-  process.env.GITHUB_ACTION = '';
+  process.env.GITHUB_ACTIONS = GITHUB_ACTIONS;
+  process.env.GITHUB_ACTOR = GITHUB_ACTOR;
 });
