9b338ee6f7d57fc4e73f00edba3e3f75f9f532b6
Zane Shannon <z@zcs.me>
2020-03-30 15:31:00 -0700

    added failing test for tagName that's more than just ^v

--- test/git.init.js
+++ test/git.init.js
@@ -99,6 +99,14 @@ test.serial('should detect include version prefix (without "v")', async t => {
   t.is(gitClient.getContext('tagName'), '1.0.1');
 });
 
+test.serial('should honor tagName configuration', async t => {
+  const gitClient = factory(Git, { options: { git: { tagName: 'TAGNAME-v${version}' } } });
+  sh.exec('git tag 1.0.0');
+  await gitClient.init();
+  await gitClient.bump('1.0.1');
+  t.is(gitClient.getContext('tagName'), 'TAGNAME-v1.0.1');
+});
+
 test.serial('should detect include version prefix (configured)', async t => {
   const gitClient = factory(Git, { options: { git: { tagName: 'v${version}' } } });
   sh.exec('git tag 1.0.0');
