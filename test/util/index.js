f31868e74837a8d21b12838ec64e3e791fae50de
Lars Kappert <lars@webpro.nl>
2021-11-14 12:40:43 +0100

    Remove unused argument in changelog method calls

--- test/util/index.js
+++ test/util/index.js
@@ -56,7 +56,7 @@ module.exports.runTasks = async plugin => {
 
   const name = (await plugin.getName()) || '__test__';
   const latestVersion = (await plugin.getLatestVersion()) || '1.0.0';
-  const changelog = (await plugin.getChangelog(latestVersion)) || null;
+  const changelog = (await plugin.getChangelog()) || null;
   const increment = getIncrement(plugin, { latestVersion });
 
   plugin.config.setContext({ name, latestVersion, latestTag: latestVersion, changelog });
