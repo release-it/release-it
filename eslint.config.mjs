ab29bcf95e4c7d79acb702e325e5e10381031993
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2024-05-22 12:11:28 -0400

    chore: Upgrade ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,54 @@
+import prettier from "eslint-plugin-prettier";
+import _import from "eslint-plugin-import";
+import { fixupConfigRules } from "@eslint/compat";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [
+    ...fixupConfigRules(compat.extends("eslint:recommended", "plugin:ava/recommended", "prettier")),
+    {
+        plugins: {
+            prettier,
+            import: _import,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.es6,
+            },
+
+            ecmaVersion: 2020,
+            sourceType: "module",
+        },
+
+        rules: {
+            "no-unused-vars": ["error", { caughtErrors: "none" }],
+            "prettier/prettier": 2,
+            "ava/no-ignored-test-files": 0,
+            "ava/no-import-test-files": 0,
+
+            "import/no-unresolved": [2, {
+                ignore: ["ava", "got"],
+            }],
+
+            "import/no-unused-modules": 2,
+
+            "import/order": [2, {
+                "newlines-between": "never",
+            }],
+        },
+    },
+];
