4e094fcf8d98c3f2d957d2f348e6dca9e2cfb2ed
Lars Kappert <lars@webpro.nl>
2025-03-17 21:55:17 +0100

    Refactor tests
    # Conflicts:
    #	test/gitlab.js
    
    # Conflicts:
    #	test/config.js
    #	test/tasks.interactive.js

--- test/stub/gitlab.js
+++ test/stub/gitlab.js
@@ -1,63 +1,85 @@
-import nock from 'nock';
+export const interceptUser = (server, { owner = 'user' } = {}, options = {}) => {
+  server.get({ url: '/user', ...options }, { status: 200, body: { id: 1, username: owner } });
+};
 
-export let interceptUser = ({ host = 'https://gitlab.com', owner = 'user' } = {}, options) =>
-  nock(host, options).get('/api/v4/user').reply(200, { id: 1, username: owner });
-
-export let interceptCollaborator = (
-  { host = 'https://gitlab.com', owner = 'user', project = 'repo', group, userId = 1 } = {},
-  options
+export const interceptCollaborator = (
+  server,
+  { owner = 'user', project = 'repo', group, userId = 1 } = {},
+  options = {}
 ) =>
-  nock(host, options)
-    .get(`/api/v4/projects/${group ? `${group}%2F` : ''}${owner}%2F${project}/members/all/${userId}`)
-    .reply(200, { id: userId, username: owner, access_level: 30 });
+  server.get(
+    {
+      url: `/projects/${group ? `${group}%2F` : ''}${owner}%2F${project}/members/all/${userId}`,
+      ...options
+    },
+    {
+      status: 200,
+      body: { id: userId, username: owner, access_level: 30 }
+    }
+  );
 
-export let interceptPublish = ({ host = 'https://gitlab.com', owner = 'user', project = 'repo', body } = {}, options) =>
-  nock(host, options)
-    .post(`/api/v4/projects/${owner}%2F${project}/releases`, body)
-    .reply(200, {
-      _links: {
-        self: `https://gitlab.com/${owner}/${project}/-/releases/${body?.tag_name ?? '1.0.0'}`
-      }
-    });
-
-export let interceptMilestones = (
-  { host = 'https://gitlab.com', owner = 'user', project = 'repo', query = {}, milestones = [] } = {},
-  options
-) =>
-  nock(host, options)
-    .get(`/api/v4/projects/${owner}%2F${project}/milestones`)
-    .query(
-      Object.assign(
-        {
-          include_parent_milestones: true
-        },
-        query
-      )
-    )
-    .reply(200, JSON.stringify(milestones));
-
-export let interceptAsset = ({ host = 'https://gitlab.com', owner = 'user', project = 'repo' } = {}) =>
-  nock(host)
-    .post(`/api/v4/projects/${owner}%2F${project}/uploads`)
-    .query(true)
-    .reply(200, (_, requestBody) => {
-      const [, name] = requestBody.match(/filename="([^"]+)/);
-      return {
-        alt: name,
-        url: `/uploads/7e8bec1fe27cc46a4bc6a91b9e82a07c/${name}`,
-        full_path: `/-/project/1234/uploads/7e8bec1fe27cc46a4bc6a91b9e82a07c/${name}`,
-        markdown: `[${name}](/uploads/7e8bec1fe27cc46a4bc6a91b9e82a07c/${name})`,
+export const interceptPublish = (server, { owner = 'user', project = 'repo', body } = {}) =>
+  server.post(
+    { url: `/projects/${owner}%2F${project}/releases`, body },
+    {
+      status: 200,
+      body: {
         _links: {
-          self: `https://gitlab.com/${owner}/${project}/-/releases/${name}`
+          self: `https://gitlab.com/${owner}/${project}/-/releases/${body?.tag_name ?? '1.0.0'}`
+        }
+      }
+    }
+  );
+
+export const interceptMilestones = (server, { owner = 'user', project = 'repo', query = {}, milestones = [] } = {}) =>
+  server.get(
+    {
+      url: `/projects/${owner}%2F${project}/milestones`,
+      query: {
+        include_parent_milestones: 'true',
+        ...query
+      }
+    },
+    {
+      status: 200,
+      body: milestones
+    }
+  );
+
+export const interceptAsset = (server, { owner = 'user', project = 'repo' } = {}) =>
+  server.post(
+    {
+      url: `/projects/${owner}%2F${project}/uploads`
+    },
+    async (request, {}) => {
+      const reader = request.body.getReader();
+      const { value } = await reader.read();
+      const bodyText = new TextDecoder().decode(value);
+      const [, name] = bodyText.match(/filename="([^"]+)"/);
+      return {
+        status: 200,
+        body: {
+          alt: name,
+          url: `/uploads/7e8bec1fe27cc46a4bc6a91b9e82a07c/${name}`,
+          full_path: `/-/project/1234/uploads/7e8bec1fe27cc46a4bc6a91b9e82a07c/${name}`,
+          markdown: `[${name}](/uploads/7e8bec1fe27cc46a4bc6a91b9e82a07c/${name})`,
+          _links: {
+            self: `https://gitlab.com/${owner}/${project}/-/releases/${name}`
+          }
         }
       };
-    });
+    }
+  );
 
-export let interceptAssetGeneric = ({ host = 'https://gitlab.com', owner = 'user', project = 'repo' } = {}) =>
-  nock(host)
-    .put(`/api/v4/projects/${owner}%2F${project}/packages/generic/release-it/2.0.1/file-v2.0.1.txt`)
-    .reply(200, () => {
-      return {
+export const interceptAssetGeneric = (server, { owner = 'user', project = 'repo' } = {}) =>
+  server.put(
+    {
+      url: `/projects/${owner}%2F${project}/packages/generic/release-it/2.0.1/file-v2.0.1.txt`
+    },
+    {
+      status: 200,
+      body: {
         message: '201 Created'
-      };
-    });
+      }
+    }
+  );
