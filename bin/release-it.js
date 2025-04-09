032c993bd288eb82c1fcdf2251820af06defc639
Lars Kappert <lars@webpro.nl>
2025-04-09 22:10:05 +0200

    Remove update-notifier

--- bin/release-it.js
+++ bin/release-it.js
@@ -1,15 +1,10 @@
 #!/usr/bin/env node
 
-import updater from 'update-notifier';
 import release from '../lib/cli.js';
-import { readJSON } from '../lib/util.js';
 import { parseCliArguments } from '../lib/args.js';
-
-const pkg = readJSON(new URL('../package.json', import.meta.url));
 
 const options = parseCliArguments([].slice.call(process.argv, 2));
 
-updater({ pkg: pkg }).notify();
 release(options).then(
   () => process.exit(0),
   ({ code }) => process.exit(Number.isInteger(code) ? code : 1)
