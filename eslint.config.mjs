41dfaaeabc720bf683e4c8daf527db9786a6adfe
Lars Kappert <lars@webpro.nl>
2025-04-18 08:17:02 +0200

    Housekeeping

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,7 +31,7 @@ export default [
     },
     rules: {
       'no-unused-vars': ['error', { caughtErrors: 'none' }],
-      'import/no-unresolved': [2, { ignore: ['@octokit/rest', '@octokit/request-error'] }],
+      'import/no-unresolved': [2, { ignore: ['@octokit/rest', '@octokit/request-error', 'c12'] }],
       'import/no-unused-modules': 2,
       'import/order': [2, { 'newlines-between': 'never' }]
     }
