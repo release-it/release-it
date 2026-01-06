f6cc8f3622995ebe98c43a8a5adb8d62b2de70b8
Lars Kappert <lars@webpro.nl>
2026-01-06 18:18:10 +0100

    Remove obsolete eslint compat packages/config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,9 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import _import from 'eslint-plugin-import-x';
-import { fixupConfigRules } from '@eslint/compat';
 import globals from 'globals';
 import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-});
 
 export default [
-  ...fixupConfigRules(compat.extends('eslint:recommended')),
+  js.configs.recommended,
   {
     plugins: {
       import: _import
