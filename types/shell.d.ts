875dba07c6216a9569c058d902d9ddb949b17283
Jochen Ulrich <jochenulrich@t-online.de>
2026-08-25 00:16:36 +0200

    Simplify Shell type

--- types/shell.d.ts
+++ types/shell.d.ts
@@ -1,26 +1,22 @@
 import { exec, SpawnOptions } from 'node:child_process';
 
+export interface CommonExecOptions {
+  write?: boolean;
+  external?: boolean;
+  cache?: boolean;
+  interactive?: boolean;
+  env?: NonNullable<Parameters<typeof exec>[1]>['env'];
+}
+
 export default interface Shell {
   exec(
     command: string,
-    options: {
-      write?: boolean;
-      external?: boolean;
-      cache?: boolean;
-      interactive?: boolean;
-      env?: NonNullable<Parameters<typeof exec>[1]>['env'];
-    },
-    context: any
+    options: CommonExecOptions,
+    context: object
   ): Promise<any>;
   exec(
     command: string[],
-    options: {
-      write?: boolean;
-      external?: boolean;
-      cache?: boolean;
-      interactive?: boolean;
-      env?: NonNullable<Parameters<typeof exec>[1]>['env'];
-    } & SpawnOptions,
-    context: any
+    options: CommonExecOptions & SpawnOptions,
+    context: object
   ): Promise<any>;
 }
